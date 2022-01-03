18d7510da1a4602f2a886136587e348efdf15f9c
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-03 17:30:48 +0100

    create banner component

---
+++ components/banner.jsx
@@ -0,0 +1,19 @@
+import styles from "./banner.module.css";
+const Banner = ({ buttonTitle, handleClick }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>Coffee</span>
+        <span className={styles.title2}>Connoisseur</span>
+      </h1>
+      <p className={styles.subTitle}>Discover your local coffee shops!</p>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} onClick={handleClick}>
+          {buttonTitle}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
