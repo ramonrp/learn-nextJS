117165ce9a6b72ed1190cc65831cab740b360b7b
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-04 17:04:09 +0100

    change result to map only to data needed

--- services/getCoffeStores.js
+++ services/getCoffeStores.js
@@ -1,9 +1,25 @@
+import { createApi } from "unsplash-js";
+
+const unsplashApi = createApi({
+  accessKey: process.env.UNSPLASH_KEY,
+});
+
+async function getCoffeStorePictures() {
+  const photos = await unsplashApi.search.getPhotos({
+    query: "coffee stores",
+    perPage: 6,
+  });
+  const unsplashResults = photos.response?.results || [];
+  return unsplashResults.map((result) => result.urls["small"]);
+}
+
 export async function getCoffeeStoresData(latLong, query, limit) {
+  const photos = await getCoffeStorePictures();
   const options = {
     method: "GET",
     headers: {
       Accept: "application/json",
-      Authorization: String(process.env.FOUR_SQUARE_KEY),
+      Authorization: process.env.FOUR_SQUARE_KEY,
     },
   };
 
@@ -12,5 +28,16 @@ export async function getCoffeeStoresData(latLong, query, limit) {
     options
   );
   const coffeStoreResult = await resp.json();
-  return coffeStoreResult.results;
+  const result = coffeStoreResult.results.map((coffeStore, index) => {
+    const { fsq_id: id, location, name } = coffeStore;
+    const { address, neighborhood } = location;
+    return {
+      id,
+      address,
+      name,
+      neighborhood: neighborhood?.[0] || null,
+      imgUrl: photos[index],
+    };
+  });
+  return result;
 }
