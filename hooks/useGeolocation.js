22a4b3fc3c752ac6eff7d6f6786442f334385ab5
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-05 10:44:50 +0100

    useContext to save latLong

--- hooks/useGeolocation.js
+++ hooks/useGeolocation.js
@@ -1,18 +1,24 @@
 import { useState } from "react";
-
+import { useNearStores } from "../context/nearCoffeStore";
 const useGeolocation = () => {
   const [status, setStatus] = useState("idle");
-  const [latLong, setLatLong] = useState("");
+  const { nearStores, setNearStores } = useNearStores();
 
   function success(position) {
     setStatus("success");
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    setLatLong(`${latitude},${longitude}`);
+    setState({
+      ...nearStores,
+      latLong: `${latitude},${longitude}`,
+    });
   }
   function error() {
     setStatus("error");
-    setLatLong("");
+    setState({
+      ...nearStores,
+      latLong: "",
+    });
   }
   function handleGeoLocation() {
     if (!navigator.geolocation) {
@@ -22,7 +28,7 @@ const useGeolocation = () => {
       navigator.geolocation.getCurrentPosition(success, error);
     }
   }
-  return { handleGeoLocation, status, latLong };
+  return { handleGeoLocation, status, latLong: nearStores.latLong };
 };
 
 export { useGeolocation };
