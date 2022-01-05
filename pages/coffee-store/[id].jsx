4f81a754e1ed0dd8da3938ceb8fd2cdba7bf02b9
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-05 17:34:43 +0100

    add get coffee store data from context

--- pages/coffee-store/[id].jsx
+++ pages/coffee-store/[id].jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { getCoffeeStoresData } from "../../services/getCoffeStores";
 import styles from "./coffe-store.module.css";
+
+import { useNearStores } from "../../context/nearCoffeStore";
 const Store = ({ coffeStoreData }) => {
-  const { name, neighborhood, address, imgUrl } = coffeStoreData;
+  const router = useRouter();
+  const [coffeStore, setCoffeStore] = useState(coffeStoreData);
+  const { id } = router.query;
+  const { nearStores } = useNearStores();
+
+  useEffect(() => {
+    if (Object.keys(coffeStore).length === 0) {
+      if (nearStores.nearStores.length > 0) {
+        setCoffeStore(nearStores.nearStores.find((store) => store.id === id));
+      }
+    }
+  }, [coffeStore, id, nearStores]);
+
+  const { name, neighborhood, address, imgUrl } = coffeStore;
 
   return (
     <div className={styles.layout}>
@@ -12,13 +29,22 @@ const Store = ({ coffeStoreData }) => {
           <a>Back to Home</a>
         </Link>{" "}
         <div>
-          <Image src={imgUrl} alt={name} width={600} height={360}></Image>
+          <Image
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1482350325005-eda5e677279b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyODgyNTN8MHwxfHNlYXJjaHwyMHx8Y29mZmVlJTIwc3RvcmVzfGVufDB8fHx8MTY0MTM3MjY0NQ&ixlib=rb-1.2.1&q=80&w=400"
+            }
+            alt={name}
+            width={600}
+            height={360}
+          ></Image>
         </div>
       </div>
       <div className={styles.col2}>
         <div className="glass">
           <p>{name}</p>
           <p>{address}</p>
+          <p>1 like</p>
           {neighborhood !== undefined > 0 && <p>{neighborhood}</p>}
         </div>
       </div>
@@ -38,7 +64,7 @@ export async function getStaticProps({ params }) {
     querySearch,
     limit
   );
-  const coffeStoreData = coffeStoresData.find((store) => store.id == id);
+  const coffeStoreData = coffeStoresData.find((store) => store.id == id) || {};
   return {
     props: { coffeStoreData },
   };
@@ -55,6 +81,6 @@ export async function getStaticPaths() {
   );
   return {
     paths: coffeStoreData.map((store) => ({ params: { id: store.id } })),
-    fallback: "blocking",
+    fallback: true,
   };
 }
