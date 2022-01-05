7ad2ae82aa9fc46a32cdc82e49a1de50a0921774
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-05 10:44:22 +0100

    add NearStores provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import "../styles/globals.css";
-
+import { NearStoreProvider } from "../context/nearCoffeStore";
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <NearStoreProvider>
       <Component {...pageProps} />
-    </>
+    </NearStoreProvider>
   );
 }
 
