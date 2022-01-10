d70f7f2e4a2b5222ae6b4f83a533f09777a04ed4
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-10 12:11:07 +0100

    add getCoffeById and addCoffeStore from airtable database

---
+++ services/airtable.js
@@ -0,0 +1,27 @@
+const Airtable = require("airtable");
+const base = new Airtable({ apiKey: process.env.AIRTABLE_KEY }).base(
+  "appg41QV4dZ7tA4Bm"
+);
+
+async function getCoffeStoreById(id) {
+  const records = await base("coffe-stores")
+    .select({
+      view: "Grid view",
+    })
+    .firstPage();
+  return records
+    .filter((record) => record.fields.id === id)
+    .map((record) => ({ airtableId: record.id, ...record.fields }));
+}
+
+async function addCoffeStore(record) {
+  return base("coffe-stores").create([
+    {
+      fields: {
+        ...record,
+      },
+    },
+  ]);
+}
+
+export { getCoffeStoreById, addCoffeStore };
