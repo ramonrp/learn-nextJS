451b127b39c53ff62ab5821d7be709407084aaaa
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-01-10 19:46:49 +0100

    refactor upvoteCoffeStore to use only id in request

--- pages/api/upvoteCoffeStore.js
+++ pages/api/upvoteCoffeStore.js
@@ -1,14 +1,24 @@
-import { updateFieldVote } from "../../services/airtable";
+import { updateFieldVote, getCoffeStoreById } from "../../services/airtable";
 export default async function (req, res) {
   const { method } = req;
   if (method !== "PUT") res.json("Method need to be PUT");
-  const { votes, id: airtableId } = req.body;
-  console.log(votes, airtableId);
-  if (!airtableId) res.json("Body must contain an id");
-  if (!votes) res.json("Body must contain a votes field");
+  const { id } = req.body;
+  if (!id) res.json("Body must contain an id");
   try {
-    const result = await updateFieldVote({ airtableId, votes });
-    res.status(200).json(result);
+    const records = await getCoffeStoreById(id);
+    if (records.length > 0) {
+      const airtableId = records[0].airtableId;
+      const newVotes = records[0].votes + 1;
+      console.log(newVotes);
+      const result = await updateFieldVote({ airtableId, votes: newVotes });
+      const minifiedRecords = result.map((record) => ({
+        airtableId: record.id,
+        ...record.fields,
+      }));
+      res.status(200).json(minifiedRecords);
+    } else {
+      res.json("id is not correct");
+    }
   } catch (err) {
     console.error(err);
   }
